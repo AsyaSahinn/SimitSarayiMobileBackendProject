ddbc6c54533152b6ae73be55e90b1fe5165bc8cf
Asya Şahin <asya.sahin@bulutera.co>
2024-04-29 16:17:30 +0300

    Created all microservice file and project file.

--- src/BuildingBlocks/EventBus/EventBus.Base/obj/Debug/net8.0/EventBus.Base.AssemblyInfo.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/obj/Debug/net8.0/EventBus.Base.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventBus.Base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3300324864e3894af84788190d0aa1ccdf8bd934")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventBus.Base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventBus.Base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
