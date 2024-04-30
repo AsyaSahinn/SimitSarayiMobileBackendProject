5beaff6ded16cbc416f3c71a9a47064c7f117e90
Asya Şahin <asya.sahin@bulutera.co>
2024-04-30 14:50:43 +0300

    EventBus project configuration completed

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -10,7 +10,9 @@ namespace EventBus.Base.Abstraction
     public interface IEventBus
     {
         void Publish(IntegrationEvent @event);
-        void Subscribe<T, TH>() where T:IntegrationEvent where TH:IIntegrationEventHandler<T>;    
-        void UnSubscribe<T, TH>() where T:IntegrationEvent where TH:IIntegrationEventHandler<T>;    
+
+        void Subscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
+
+        void UnSubscribe<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
     }
 }
